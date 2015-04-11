b3d35588c8a850493a0e3af48ae4f796baec5d06
MMX <trudosm@plu.edu>
2015-04-10 19:45:22 -0700

    Increased Effective Zero Value
    The slip parameter had a habit of fluctuating wildly near the end of the
    time interval. It is possible that the small values of velocity were
    causing this to happen. In order to remedy this, I increased the value
    of effective zero that the program would stop calculations.

--- EulerMethod.m
+++ EulerMethod.m
@@ -1,72 +1,92 @@
 %% Euler Method of Computing
+%hold on
+x_max_array = [];
+t_max_array = [];
+u_max_array = [];
+l_max_array = [];
+
 t_o = 0;        % s
 g = 9.81;       % m/s^2
 J = 0.8;        % kg*m^2 - 
 B = 0.08;        % kg*m^2/s
-tau = 500;       % N*m - braking torque
+%tau = 500;       % N*m - braking torque
 l_o = 0.22;
 u_o = 0.9;
 M = 220;        % kg - Normal Mass
 r = 0.28;       % m - radius of wheel
 delta_t = 0.001; % s - time step
-
-t = t_o;
-
 v_o = 25; %m/s
-v_i = v_o;
 
-w_o = v_i / r;
-w_i = w_o;
+for tau=linspace(562, 564, 10)
 
-x = 0;
-
-w_array = [];
-v_array = [];
-t_array = [];
-l_array = [];
-u_array = [];
-x_array = [];
-
-fprintf('%5s \t %10s \t %10s \t %10s \t %10s \t %10s \n', 't', 'v', 'w', 'u(l)', 'l', 'x')
-mult = 1;
-while ( v_i > 0.1 )
+    t = t_o;
+    v_i = v_o;
+    w_o = v_i / r;
+    w_i = w_o;
+    x = 0;
+    mult = 1;
     
-        l = 1 - ( (r*w_i) / v_i );
-        if ( l > 1 ) 
-            l = 1;
-        elseif ( l < 0 )
-            l = 0;
-        end
-        
-        u = 2*u_o*( (l_o * l) / ( (l_o^2) + (l^2) ) );
-        
-        v_i1 = v_i - u * g * delta_t;
-        w_i1 = mult*(delta_t/J)*( u * M * g * r - B * w_i - tau ) + w_i;
-        if( w_i1 <= 0 )
-            mult = 0;
-        else
-            mult = 1;
-        end
-        
-        x_1 = v_i*delta_t + x;
+    w_array = [];
+    v_array = [];
+    t_array = [];
+    l_array = [];
+    u_array = [];
+    x_array = [];
+
+    fprintf('%5s \t %10s \t %10s \t %10s \t %10s \t %10s \n', 't', 'v', 'w', 'u(l)', 'l', 'x')
     
-        fprintf('%5.2f \t %10.4f \t %10.4f \t %10.4f \t %10.4f \t %10.4f\n', t, v_i1, w_i1, u, l, x)
+
+    while ( v_i > 1 )
     
-        t = t + delta_t;
-        v_i = v_i1;
-        w_i = w_i1;
-        x = x_1;
-        
-        
-        w_array = cat(1, w_array, w_i);
-        v_array = cat(1, v_array, v_i);
-        t_array = cat(1, t_array, t);
-        l_array = cat(1, l_array, l);
-        u_array = cat(1, u_array, u);
-        x_array = cat(1, x_array, x);
+            l = 1 - ( (r*w_i) / v_i );
+%             if ( l > 1 ) 
+%                 l = 1;
+%             elseif ( l < 0 )
+%                 l = 0;
+%             end
+
+            u = 2*u_o*( (l_o * l) / ( (l_o^2) + (l^2) ) );
+
+            v_i1 = v_i - u * g * delta_t;
+
+            w_i1 = (delta_t/J)*( u * M * g * r - 0 * w_i - tau ) + w_i;
+            if( w_i1 <= 0 )
+                mult = 0;
+            else
+                mult = 1;
+            end
+            w_i1 = mult*w_i1;
+
+            x_1 = v_i*delta_t + x;
+
+            fprintf('%5.2f \t %10.4f \t %10.4f \t %10.4f \t %10.4f \t %10.4f\n', t, v_i1, w_i1, u, l, x)
+
+            t = t + delta_t;
+            v_i = v_i1;
+            w_i = w_i1;
+            x = x_1;
+
+            plot( t_array, l_array )
+            w_array = cat(1, w_array, w_i);
+            v_array = cat(1, v_array, v_i);
+            t_array = cat(1, t_array, t);
+            l_array = cat(1, l_array, l);
+            u_array = cat(1, u_array, u);
+            x_array = cat(1, x_array, x);
+            
+    end;
+
+    %plot( t_array, x_array )
+    x_max_array = cat(1, x_max_array, [max( x_array ), tau]);
+    t_max_array = cat(1, t_max_array, [max( t_array ), tau]);
+    u_max_array = cat(1, u_max_array, [max( u_array ), tau]);
+    l_max_array = cat(1, l_max_array, [max( l_array ), tau]);
 end;
-
-plot( t_array, u_array )
+plotx =  x_max_array(:,1)
+ploty = x_max_array(:,2)
+plot( plotx, ploty )
+min(t_max_array)
+max(u_max_array)
 
 %% Midpoint  Approximation - Computing Using 2 Timesteps 
 t_o = 0;        % s
